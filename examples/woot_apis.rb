d2915e2fbc298fa5a49ffbd1e150f84a9029a050
Sean Kelly <sean.kelly@hipponot.com>
2013-10-24 11:45:12 -0600

    emacs

--- examples/woot_apis.rb
+++ examples/woot_apis.rb
@@ -1,13 +1,14 @@
 require_relative '../lib/zapt'
 
+package do
+  names %w{emacs23}
+end
+
 git do
   repo 'https://github.com/hipponot/kudu.git'
   dir ENV['HOME']
 end
 
-# package do
-#   names %w{memcached nginx}
-# end
 
 # gem do
 #   names %w{popen4 json}
