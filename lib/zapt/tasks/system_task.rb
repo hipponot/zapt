b9959015bd6a337c1d8f9959386410bda9988950
vagrant <vagrant@precise32.(none)>
2013-10-28 16:28:27 +0000

    location woot_cms

--- lib/zapt/tasks/system_task.rb
+++ lib/zapt/tasks/system_task.rb
@@ -14,19 +14,20 @@ module Zapt
       @cmds ||= []
     end
 
-    def commands cmds, working_dir:ENV['HOME']
+    def commands cmds, working_dir:ENV['HOME'], user:nil
       cmds.each do |cmd| 
-        run_cmd cmd, working_dir
+        run_cmd cmd, working_dir, user
       end
     end
 
-    def command cmd, working_dir:ENV['HOME']
-      run_cmd cmd, working_dir
+    def command cmd, working_dir:ENV['HOME'], user:nil
+      run_cmd cmd, working_dir, user
     end
 
     private 
     
-    def run_cmd cmd, working_dir
+    def run_cmd cmd, working_dir, user
+      cmd.insert(0,"sudo su #{user} -l -c ") unless user.nil?
       cmd.insert(0,"cd #{working_dir};")
       Zapt.system(cmd) 
     end
