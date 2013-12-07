b9923bd3b43ca95b409889758bfcfb18e7d36062
Sean Kelly <sean.kelly@simbulus.com>
2013-12-07 12:40:58 -0700

    adding cluster remoting

--- lib/zapt/commands/runtasks.rb
+++ lib/zapt/commands/runtasks.rb
@@ -5,18 +5,28 @@ module Zapt
     desc "runtask", "run the tasks specified in runlist"
     method_option :tasks, :aliases => "-t", :type=>:string, :default=>'tasks.rb', :required=>false, :desc => "Task file"
     method_option :runlist, :aliases => "-r", :type=>:array, :required=>true, :desc => "Run list"
+    method_option :cluster, :aliases => "-c", :type=>:string, :required=>false, :desc => "Specify cluster on which to run task"
     def runtask
-      # quiet zapt for this command
       task_file = options[:tasks]
       Zapt.load_and_eval task_file
       options[:runlist].each do |task|
-        unless Zapt::Tasks.registry.has_key? task
-          $logger.error("No such task #{task}")
-          exit(1)
+        $logger.error("No such task #{task}") and exit(1) unless Zapt::Tasks.registry.has_key? task
+        if options[:cluster]
+          cluster = options[:cluster]
+          $logger.error("Can't find cluster definition #{cluster}") and exit(1) unless File.exist?(cluster) 
+          nodes = YAML::load(IO.read(cluster))[:nodes]
+          nodes.each do |node|
+            puts node[:ip]
+            task = Zapt::Tasks.registry[task]
+            remote_task = ShellTask.new
+            remote_dir = File.basedir(File.join('zcripts', File.expand_path('tasks.rb').split('zcripts/')[1]))
+            remote_task.command "cd #{remote_dir}; rvmsudo zapt runtask -r #{task}", host:node[:ip], user:node[:user]
+          end
+        else
+          "running #{task}"
+          $logger.info "running #{task}"
+          Zapt::Tasks.registry[task].run
         end
-        "running #{task}"
-        $logger.info "running #{task}"
-        Zapt::Tasks.registry[task].run
       end
     end
   end
