09b870f973fabb4a0e2cf3db3096ad43fcf6f7c8
Sean Kelly <sean.kelly@simbulus.com>
2014-08-08 15:47:44 -0600

    added yes?

--- lib/zapt.rb
+++ lib/zapt.rb
@@ -44,6 +44,17 @@ module Zapt
       gets.tap{|text| text.strip! if text}
     end
 
+    def yes?(prompt = 'Continue?', default = true)
+      a = ''
+      s = default ? '[Y/n]' : '[y/N]'
+      d = default ? 'y' : 'n'
+      until %w[y n].include? a
+        a = ask("#{prompt} #{s} ") { |q| q.limit = 1; q.case = :downcase }
+        a = d if a.length == 0
+      end
+      a == 'y'
+    end
+
   end
 end
 
