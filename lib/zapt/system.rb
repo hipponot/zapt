1199fa1739e93f927f48a3e3a3360ff310d8440e
Sean Kelly <sean.kelly@simbulus.com>
2016-07-07 10:34:44 -0600

    system.rb

--- lib/zapt/system.rb
+++ lib/zapt/system.rb
@@ -6,9 +6,9 @@ module Zapt
       if user and !host
         cmd = "sudo su #{user} -l -c \"#{cmd}\"" 
       elsif /10\.0\.10\.\d\d/ =~ host
-        cmd = "ssh -o \"StrictHostKeyChecking no\" vagrant@#{host} \"#{cmd}\"" if host
+        cmd = "stdbuf -i0 -o0 -e0 ssh -o \"StrictHostKeyChecking no\" vagrant@#{host} \"#{cmd}\"" if host
       else
-        cmd = "ssh -o \"StrictHostKeyChecking no\" -i #{pem} #{user}@#{host} \"#{cmd}\"" if host
+        cmd = "stdbuf -i0 -o0 -e0 ssh -o \"StrictHostKeyChecking no\" -i #{pem} #{user}@#{host} \"#{cmd}\"" if host
       end
       rval = ""
       exit_status = nil
