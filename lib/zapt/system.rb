48928779f9a5d8d986b87d7c117e9835de777df4
Sean Kelly <sean.kelly@simbulus.com>
2017-11-08 12:17:25 -0700

    retire popen3 ??

--- lib/zapt/system.rb
+++ lib/zapt/system.rb
@@ -2,7 +2,7 @@ require 'open3'
 module Zapt
   class << self
 
-    def system cmd, user=nil, host=nil, pem=nil, quiet=false 
+    def system cmd, user=nil, host=nil, pem=nil, quiet=false
       if user and !host
         cmd = "sudo su #{user} -l -c \"#{cmd}\""
       elsif /10\.0\.10\.\d\d/ =~ host
@@ -13,16 +13,18 @@ module Zapt
       rval = ""
       exit_status = nil
       $logger.info "Running command: #{cmd}" unless quiet
-      Open3::popen3(cmd) do |stdin, stdout, stderr, status|
-        stdout.each do |line|
-          $logger.info line.chomp unless quiet
-          rval += line
-        end
-        stderr.each do |line|
-          $logger.warn line.chomp
-        end
-        exit_status = status.value.success?
-      end
+      rval = `#{cmd}`; exit_status=$?
+      puts rval
+      # Open3::popen3(cmd) do |stdin, stdout, stderr, status|
+      #   stdout.each do |line|
+      #     $logger.info line.chomp unless quiet
+      #     rval += line
+      #   end
+      #   stderr.each do |line|
+      #     $logger.warn line.chomp
+      #   end
+      #   exit_status = status.value.success?
+      # end
       return rval, exit_status
     end
   end
