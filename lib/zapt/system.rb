cd1a256bf34d6e82b581851e4962382cec603342
Sean <sean.kelly@wootmath.com>
2022-10-07 09:14:33 -0600

    -

--- lib/zapt/system.rb
+++ lib/zapt/system.rb
@@ -3,46 +3,45 @@ require 'open3'
 USE_BACKTICKS = true
 
 module Zapt
-
-    def system cmd, user=nil, host=nil, pem=nil, quiet=false, ignore_failure=false
-      # switch user
-      if user and !host
-        cmd = "sudo su #{user} -l -c \"#{cmd}\""
-        #ToDo: use ipaddr private? when we upgrade to 2.5.x or better
-      # run on remove host with no pem
-      elsif  host && pem.nil?
-        cmd = "ssh -tt -o \"StrictHostKeyChecking no\" #{user}@#{host} \"#{cmd}\""
-      # run on remove host with pem
-      elsif host
-        cmd = "ssh -tt -o \"StrictHostKeyChecking no\" -i #{pem} #{user}@#{host} \"#{cmd}\""
-      end
-      rval = ""
-      exit_status = nil
-      $logger.info "Running command: #{cmd}" unless quiet
-      # alternatively use Open3.capture3 (this caused hangs but maybe -tt fixed the issue?)
-      if USE_BACKTICKS
-       rval = `#{cmd}`; exit_status=($?.exitstatus == 0)
-       if(exit_status)
-         $logger.info(rval);
-       else
-         $logger.error(rval) unless ignore_failure
-       end
-      else
-        Open3::popen3(cmd) do |stdin, stdout, stderr, status|
-          stdout.each do |line|
-            $logger.info line.chomp unless quiet
-            stdout.flush
-            rval += line
-          end
-          stderr.each do |line|
-            $logger.warn line.chomp
-            stderr.flush
-          end
-          exit_status = status.value.success?
-        end
-      end
-      return rval, exit_status
+  def system cmd, user=nil, host=nil, pem=nil, quiet=false, ignore_failure=false
+    # switch user
+    if user and !host
+      cmd = "sudo su #{user} -l -c \"#{cmd}\""
+      #ToDo: use ipaddr private? when we upgrade to 2.5.x or better
+    # run on remove host with no pem
+    elsif  host && pem.nil?
+      cmd = "ssh -tt -o \"StrictHostKeyChecking no\" #{user}@#{host} \"#{cmd}\""
+    # run on remove host with pem
+    elsif host
+      cmd = "ssh -tt -o \"StrictHostKeyChecking no\" -i #{pem} #{user}@#{host} \"#{cmd}\""
     end
+    rval = ""
+    exit_status = nil
+    $logger.info "Running command: #{cmd}" unless quiet
+    # alternatively use Open3.capture3 (this caused hangs but maybe -tt fixed the issue?)
+    if USE_BACKTICKS
+      rval = `#{cmd}`; exit_status=($?.exitstatus == 0)
+      if(exit_status)
+        $logger.info(rval);
+      else
+        $logger.error(rval) unless ignore_failure
+      end
+    else
+      Open3::popen3(cmd) do |stdin, stdout, stderr, status|
+        stdout.each do |line|
+          $logger.info line.chomp unless quiet
+          stdout.flush
+          rval += line
+        end
+        stderr.each do |line|
+          $logger.warn line.chomp
+          stderr.flush
+        end
+        exit_status = status.value.success?
+      end
+    end
+    return rval, exit_status
   end
 end
+
 
