0da507907227becb371a856cfd82a3938f145122
Sean <sean.kelly@wootmath.com>
2023-01-31 16:24:52 -0700

    -

--- lib/zapt/commands/sync.rb
+++ lib/zapt/commands/sync.rb
@@ -16,7 +16,7 @@ module Zapt
     method_option :yes, :aliases => "-y", :type=>:boolean, :required=>false, :desc => "yes to all prompts"
     desc "sync", "print version"
     def sync
-
+      puts BANNER.yellow
       cluster = options[:cluster]
       unless File.exist? cluster
         cluster = File.join(ENV['HOME'], 'dev/vega/zcripts/common/cluster_defs/', "#{cluster}.yaml")
@@ -31,7 +31,7 @@ module Zapt
         user = node[:user]
         hosts << { ip:ip, user:user }
       end
-
+      puts
       # sync zapt
       return if handle_zapt_has_local_mods # abort if local mods or unpushed changes
       handle_local_zapt_is_out_of_date
@@ -43,78 +43,8 @@ module Zapt
       handle_remote_zcripts_are_out_of_date
 
     end
-      # $logger.error("Can't find cluster definition #{cluster}") and exit(1) unless File.exist?(cluster)
-      # local_build = "git pull; gem build zapt.gemspec; gem install zapt-1.0.1.gem"
-      # $logger.warn("Running: #{local_build}")
-      # rval, = system(local_build)
-
-      # @cluster_conf = YAML::load(IO.read(cluster))
-      # hosts = []
-      # cluster_conf[:nodes].each do |node|      #   unless File.exist? cluster
-      #     cluster = File.join(ENV['HOME'], 'dev/vega/zcripts/common/cluster_defs/', "#{cluster}.yaml")
-      #   end
-
-      #   ip = Zapt.is_aws_vpn? ? node[:internal_ip] : node[:public_ip]
-      #   user = node[:user]
-      #   hosts << { ip:ip, user:user }
-      # end
-      #     hub
-      #     ans = no_prompt ? true : (yes? "Local zapt version differs from (github) origin, do you want to continue with sync?")
-      #     unless ans
-      #       $logger.info("You can rerun this command after pulling remote changes or pushing local changes")
-      #       return
-      #     end
-      #   end
-
-      #   local_hash = `find #{LOCAL_ZAPT_DIR}/lib -type f -name '*.rb' | sort -d | xargs cat | md5sum`.split(/\s+/).first
-      #   # either the full path or just the name
-      #   cluster = options[:cluster]
-      #   unless File.exist? cluster
-      #     cluster = File.join(ENV['HOME'], 'dev/vega/zcripts/common/cluster_defs/', "#{cluster}.yaml")
-      #   end
-      #   $logger.error("Can't find cluster definition #{cluster}") and exit(1) unless File.exist?(cluster)
-      #   local_build = "git pull; gem build zapt.gemspec; gem install zapt-1.0.1.gem"
-      #   $logger.warn("Running: #{local_build}")
-      #   rval, = system(local_build)
-
-      #   @cluster_conf = YAML::load(IO.read(cluster))
-      #   hosts = []
-      #   cluster_conf[:nodes].each do |node|
-      #     ip = Zapt.is_aws_vpn? ? node[:internal_ip] : node[:public_ip]
-      #     user = node[:user]
-      #     hosts << { ip:ip, user:user }
-      #   end
-
-      #   # pem from top level cluster config
-      #   remote_hash_cmd = "find #{REMOTE_ZAPT_DIR}/lib -type f -name '*.rb' | sort -d | xargs cat | md5sum"
-
-
-
-      #   hosts.each_with_index do |host|
-      #     rval, = Zapt.system(remote_hash_cmd, host[:user], host[:ip], pem)
-      #     remote_hash = rval.split(/\s+/).first
-      #     if local_diff_from_
-      #       github
-      #       unless (local_hash == remote_hash)
-      #         $logger.warn("local and #{cluster_conf[:name]} cluster versions of zapt source differ")
-      #         ans = no_prompt ? true : (yes? "Do you want to sync zapt to cluster #{cluster_conf[:name]} (Y/N)")
-      #         return unless ans
-      #         update_cluster_zapt_from_local(host, pem)
-      #       end
-      #     else
-      #       $logger.warn("local and github versions of zapt are the same")
-      #       update_cluster_zapt_from_github(host, pem)
-      #     end
-      #   end
-      # }
-      # $logger.warn("Running rsync_zcripts task")
-      # Dir.chdir(File.join(ENV['HOME'], 'dev/vega/zcripts/cluster')) {
-      #   cmd = %Q{zapt runtask -r rsync_zcripts -a "{cluster_name:'#{cluster_conf[:name]}'}"}
-      #   system(cmd)
-      # }
 
     private
-
 
     def wrap(s, width=78)
       s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
