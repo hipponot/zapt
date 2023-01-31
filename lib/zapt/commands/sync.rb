bd492d46c971bb19d5a807a2536d4569900633e2
Sean <sean.kelly@wootmath.com>
2023-01-31 16:20:30 -0700

    -

--- lib/zapt/commands/sync.rb
+++ lib/zapt/commands/sync.rb
@@ -215,17 +215,9 @@ module Zapt
       puts BANNER.yellow
       local_hash = `find #{LOCAL_ZCRIPTS_DIR} -type f | grep -v cluster_defs | sort -d | xargs cat | md5sum`.chomp
       hosts.each do |host|
-        remote_cmd = "find #{REMOTE_ZCRIPTS_DIR} -type f | grep -v cluster_defs | sort -d | xargs cat | md5sum".chomp
-        remote_hash,  = Zapt.system(remote_cmd, host[:user], host[:ip], pem, true) # quiet and ignore_failure
-        if(remote_hash != local_hash)
-          puts wrap("Remote zcripts out of sync with local")
-          Dir.chdir(File.join(ENV['HOME'], 'dev/vega/zcripts/cluster')) {
-            cmd = %Q{zapt runtask -r rsync_zcripts -a "{cluster_name:'#{cluster_conf[:name]}'}"}
-            system(cmd)
-          }
-        else
-          puts wrap("Remote zcripts are in sync with local")
-        end
+        cmd = %Q{rsync  -arc -e "ssh -i #{pem} -l #{host[:user]}" #{LOCAL_ZCRIPTS_DIR} #{host[:user]}@#{host[:ip]}:. --exclude "common/cluster_defs/*"}
+        rval = `#{cmd}`
+        puts rval
       end
       puts BANNER.yellow
     end
