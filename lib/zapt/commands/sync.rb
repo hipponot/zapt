63e0a64e1e56ac906c4ece78a6233db6bbd76993
Sean <sean.kelly@wootmath.com>
2023-01-31 16:53:01 -0700

    -

--- lib/zapt/commands/sync.rb
+++ lib/zapt/commands/sync.rb
@@ -142,6 +142,7 @@ module Zapt
       local_hash = `find #{LOCAL_ZCRIPTS_DIR} -type f | grep -v cluster_defs | sort -d | xargs cat | md5sum`.chomp
       hosts.each do |host|
         cmd = %Q{rsync  -arc -e "ssh -i #{pem} -l #{host[:user]}" #{LOCAL_ZCRIPTS_DIR} #{host[:user]}@#{host[:ip]}:. --exclude "common/cluster_defs/*"}
+        puts cmd
         rval = `#{cmd}`
         if rval.empty?
           puts wrap("Remote zcripts are up to date")
