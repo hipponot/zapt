7ae5b0baeea4101598027001e44293ae0cf9fade
Sean <sean.kelly@wootmath.com>
2023-01-31 17:03:37 -0700

    -

--- lib/zapt/commands/sync.rb
+++ lib/zapt/commands/sync.rb
@@ -141,7 +141,8 @@ module Zapt
       puts wrap("Checking if remote zcripts need rsync:\n", 80)
       local_hash = `find #{LOCAL_ZCRIPTS_DIR} -type f | grep -v cluster_defs | sort -d | xargs cat | md5sum`.chomp
       hosts.each do |host|
-        cmd = %Q{rsync  -n -arc -e "ssh -i #{pem} -l #{host[:user]}" #{LOCAL_ZCRIPTS_DIR} #{host[:user]}@#{host[:ip]}:. --exclude "common/cluster_defs/*"}
+        cmd = %Q{rsync  -n -varc -e "ssh -i #{pem} -l #{host[:user]}" #{LOCAL_ZCRIPTS_DIR} #{host[:user]}@#{host[:ip]}:. --exclude "common/cluster_defs/*"}
+        puts cmd
         rval = `#{cmd}`
         if rval.empty?
           puts wrap("Remote zcripts are up to date")
