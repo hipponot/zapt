666a0c2e0b0f29995dc2fa304eaec9f7dfb22790
Sean <sean.kelly@wootmath.com>
2023-10-11 16:03:31 -0600

    -

--- lib/zapt/tasks/shell_task.rb
+++ lib/zapt/tasks/shell_task.rb
@@ -15,24 +15,24 @@ module Zapt
       super
     end
 
-    def commands cmds, working_dir:nil, user:nil, host:nil, pem:nil, quiet:false, ignore_failure:false, dryrun:false
+    def commands cmds, working_dir:nil, user:nil, host:nil, pem:nil, quiet:false, ignore_failure:false, dryrun:false, capture:false
       rval = []
       cmds.each do |cmd|
         rval << run_cmd(cmd, working_dir, user, host, pem, quiet, dryrun, ignore_failure)
       end
     end
 
-    def command cmd, working_dir:nil, user:nil, host:nil, pem:nil, quiet:false, ignore_failure:false, dryrun:false
-      rval, status = run_cmd cmd, working_dir, user, host, pem, quiet, dryrun, ignore_failure
+    def command cmd, working_dir:nil, user:nil, host:nil, pem:nil, quiet:false, ignore_failure:false, dryrun:false, capture:false
+      rval, status = run_cmd cmd, working_dir, user, host, pem, quiet, dryrun, ignore_failure, capture
       raise Error.new "Command bad exit status #{cmd}" unless status or ignore_failure
       return rval, status
     end
 
     private
 
-    def run_cmd cmd, working_dir, user, host, pem, quiet, dryrun, ignore_failure
+    def run_cmd cmd, working_dir, user, host, pem, quiet, dryrun, ignore_failure, capture
       cmd.insert(0,"cd #{working_dir};") if working_dir
-      return Zapt.system(cmd, user, host, pem, quiet, ignore_failure) unless dryrun
+      return Zapt.system(cmd, user, host, pem, quiet, ignore_failur, capture) unless dryrun
       puts "dryrun: #{cmd}"
       return nil, 0
     end
